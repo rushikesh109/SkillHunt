444a3538a02d21fec9a55a5f764c25b4dfa749ae
rushikesh109 <mangrulerushikesh2003@gmail.com>
2024-09-24 17:31:39 +0530

    search and filter added

--- src/pages/job-listing.jsx
+++ src/pages/job-listing.jsx
@@ -1,7 +1,12 @@
 import { getJobs } from '@/api/api_jobs'
+import { getCompanies } from '@/api/apiCompanies'
 import JobCard from '@/components/job-card'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import useFetch from '@/hooks/use-fetch'
 import { useUser } from '@clerk/clerk-react'
+import { State } from 'country-state-city'
 import { useEffect, useState } from 'react'
 import { BarLoader } from 'react-spinners'
 
@@ -21,22 +26,96 @@ const {
   searchQuery,
 })
 
+const {
+  fn: fnCompanies,
+  data: companies,
+} = useFetch(getCompanies)
+
+
+useEffect(()=> {
+  if(isLoaded) fnCompanies()
+}, [isLoaded])
 
 useEffect(()=>{
- if(isLoaded) fnJobs();
-}, [isLoaded,location,
-  company_id,
-  searchQuery,])
+  if(isLoaded) fnJobs()
+}, [isLoaded, location, company_id, searchQuery])
 
+const handleSearch = (e)=>{
+e.preventDefault()
+let formData = new FormData(e.target)
 
+const query = formData.get("search-query")
+if(query) setSearchQuery(query)
+   }
+
+   const clearFilters = () => {
+    setSearchQuery("")
+    setCompany_id("")
+    setLocation("")
+   }
+  
   if(!isLoaded) {
     return <BarLoader className='mb-4' width={"100%"} color="#36d7b7" />
   }
+
   return (
     <div>
      <h1 className='gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8'> Latest Jobs
      </h1> 
-     {/* Add filters here */}
+     
+<form onSubmit={handleSearch} className='h-14 flex w-full gap-2 items-center mb-3' >
+<Input
+type='text'
+placeholder="Search Jobs by Title..."
+name="search-query"
+className="h-full flex-1 px-4 text-md"
+/>
+<Button type =" submit" className="h-full sm:w-28" variant="blue">
+  Search
+</Button>
+</form>
+
+<div className='flex felx-col sm:flex-row gap-2'>
+<Select value= {location} onValueChange={(value)=> setLocation(value)}>
+  <SelectTrigger>
+    <SelectValue placeholder="Filter by Location" />
+  </SelectTrigger>
+  <SelectContent>
+    \<SelectGroup>
+      {State.getStatesOfCountry("IN").map(({name})=>{
+        return (
+          <SelectItem key={name} value= {name}>
+            {name}
+          </SelectItem>
+        )
+      })}
+    </SelectGroup>
+  </SelectContent>
+</Select>
+
+<Select value= {company_id} onValueChange={(value)=> setCompany_id(value)}>
+  <SelectTrigger>
+    <SelectValue placeholder="Filter by Company" />
+  </SelectTrigger>
+  <SelectContent>
+    <SelectGroup>
+      {companies.map(({name, id})=>{
+        return (
+          <SelectItem key={name} value= {id}>
+            {name}
+          </SelectItem>
+        )
+      })}
+    </SelectGroup>
+  </SelectContent>
+</Select>
+<Button onClick = {clearFilters} variant="destructive" className='sm:w-1/2'>
+Clear Filters
+</Button>
+</div>
+
+
+
      {loadingJobs && (<BarLoader className='mt-4' width={"100%"} color="#36d7b7" />)}
 
      {loadingJobs === false && (
